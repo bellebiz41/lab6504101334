ace02d80df4735eb043730938d4dd6af767e8610
bellebiz41 <bellebiz41@gmail.com>
2023-09-08 22:28:56 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="app">
+    <h1>Rock Paper Scissors game</h1>
+    
+    
+    <GameComponent />
+  </div>
+</template>
+
+<script>
+import GameComponent from './components/game.vue';
+
+export default {
+  components: {
+    GameComponent,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Thai+Looped:wght@200;900&family=Noto+Sans:wght@500&family=Space+Mono:ital,wght@0,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mali:ital,wght@1,700&family=Noto+Sans+Thai+Looped:wght@200;900&family=Noto+Sans:wght@500&family=Space+Mono:ital,wght@0,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mali:ital,wght@1,700&family=Noto+Sans+Thai+Looped:wght@200;900&family=Noto+Sans:wght@500&family=Rubik+Iso&family=Space+Mono:ital,wght@0,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap');
+#app {
+  text-align: center;
+  color: #000000;
+  margin-top: 0px;
+  background-color: #FFF4C7;
+  width: 70%; /* ความกว้างเต็มหน้าจอ */
+  height: 110dvh; /* ความสูงเต็มหน้าจอ */
+  display: flex;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 100px;
+  font-family: 'Rubik Iso', cursive;
+}
+h2 {
+  font-family: 'Share Tech Mono', monospace;
+  font-size: 30px;
+}
+h3 {
+  font-family: 'Mali', cursive;
+  font-size: 50px;
+
+}
+.player1-name {
+  font-size: 34px;
+  color: #CD5C5C;
+  font-family: 'Press Start 2P', cursive;
+}
+
+
+.player2-name {
+  font-size: 34px;
+  color: #CD5C5C; 
+  font-family: 'Press Start 2P', cursive;
+}
+
+
+.player-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  
+  border-radius: 50px; 
+  
+  
+}
+
+.reset-button {
+  margin-left: 70px; 
+  background-color: #FEB9D0; 
+  color: #000000; 
+  border-radius: 50px; 
+  font-family: 'Mali', cursive;
+  font-size: 30px;
+}
+
+.reset-button:hover {
+  background-color: #ab96e5; 
+}
+.round-button:hover {
+  background-color: #b4538fee; 
+}
+
+.round-button {
+  
+  background-color: #96CFCF; 
+  color: #000000;
+  border-radius: 50px; 
+  font-family: 'Mali', cursive;
+  font-size: 30px;
+}
+
+.score-container {
+  background-color: #E57A72; 
+  color: #000000; 
+  padding: 3px; 
+  margin-top: 15px; 
+  text-align: center; 
+  border-radius: 50px; 
+  border: 2px solid #B19CD8; 
+  max-width: 300px; 
+  margin: 15px auto;
+  font-family: 'Share Tech Mono', monospace;
+}
+
+.score-text {
+  margin: 0; 
+}
+.player1-image img{
+  width: 50px;
+  height: 50px;
+}
+</style>
+
+
+
---
+++ src/components/game.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+      <div class="player-container">
+        <div>
+          <h2 class="player1-name">Player[AI]</h2>
+          <div>
+            <img v-if="player1Choice === 'ค้อน'" src="./assets/3.png" :width="300" alt="ค้อน" />
+            <img v-if="player1Choice === 'กระดาษ'" src="./assets/2.png" :width="300" alt="กระดาษ" />
+            <img v-if="player1Choice === 'กรรไกร'" src="./assets/1.png" :width="300" alt="กรรไกร" />
+          </div>
+          
+          
+        </div>
+  
+        <div>
+          <h2 class="player2-name">Player[bellaz]</h2>
+          <div>
+            <img v-if="player2Choice === 'ค้อน'" src="./assets/3.png" :width="300" alt="ค้อน" />
+            <img v-if="player2Choice === 'กระดาษ'" src="./assets/2.png" :width="300" alt="กระดาษ" />
+            <img v-if="player2Choice === 'กรรไกร'" src="./assets/1.png" :width="300" alt="กรรไกร" />
+          </div>
+          
+          
+        </div>
+  
+      </div>
+  <br>
+  <br>
+      <div>
+        <button @click="playRound" class="round-button">เป่ายิ้งฉุบบบบ</button>
+        <button @click="resetGame" class="reset-button">เริ่มใหม่ ไม่ยอมแพ้หรอกกก</button>
+      </div>
+  <br>
+      <div class="score-container"> 
+      <h2 class="score-text">SCORE {{ player1Score }} : {{ player2Score }}</h2> </div>
+    </div>
+  </template>
+  
+  <style>
+  
+  </style>
+  
+  
+  
+  <script>
+  import { ref } from 'vue';
+  
+  export default {
+    data() {
+      return {
+        player1Score: 0,
+        player2Score: 0,
+        gameResult1: '',
+        gameResult2: '',
+        player1Choice: '', 
+        player2Choice: '', 
+        choices: ['ค้อน', 'กระดาษ', 'กรรไกร'],
+      };
+    },
+    methods: {
+      playRound() {
+        const player1Index = Math.floor(Math.random() * this.choices.length);
+        const player2Index = Math.floor(Math.random() * this.choices.length);
+  
+        const player1Choice = this.choices[player1Index];
+        const player2Choice = this.choices[player2Index];
+  
+        if (player1Choice === player2Choice) {
+          this.gameResult1 = 'เสมอ ';
+          this.gameResult2 = 'เสมอ ';
+        } else if (
+          (player1Choice === 'ค้อน' && player2Choice === 'กรรไกร') ||
+          (player1Choice === 'กระดาษ' && player2Choice === 'ค้อน') ||
+          (player1Choice === 'กรรไกร' && player2Choice === 'กระดาษ')
+        ) {
+          this.gameResult1 = 'ชนะ ';
+          this.gameResult2 = 'แพ้ ';
+          this.player1Score++;
+        } else {
+          this.gameResult1 = 'แพ้ ';
+          this.gameResult2 = 'ชนะ ';
+          this.player2Score++;
+        }
+        this.player1Choice = player1Choice;
+        this.player2Choice = player2Choice;
+      },
+      resetGame() {
+        this.player1Score = 0;
+        this.player2Score = 0;
+        this.gameResult1 = '';
+        this.gameResult2 = '';
+        this.player1Choice = '';
+        this.player2Choice = '';
+      },
+    },
+  };
+  
+  </script>
+  
